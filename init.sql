0305b59ab2cee8e8005f71d18cc898f48048480a
Joel Jaquez <123708276+jaq23369@users.noreply.github.com>
2025-04-23 18:15:11 -0600

    Creación de la tabla para el register

--- init.sql
+++ init.sql
@@ -11,3 +11,18 @@ INSERT INTO login_credentials (email, password) VALUES ('pablo456@gmail.com', 'h
 INSERT INTO login_credentials (email, password) VALUES ('carlossanchez@icloud.com', 'donjuan217');
 INSERT INTO login_credentials (email, password) VALUES ('lauraramirez@icloud.com', 'capi789');
 INSERT INTO login_credentials (email, password) VALUES ('nbmaster@gmail.com', 'noobmaster69');
+
+
+CREATE TABLE IF NOT EXISTS user_details (
+    id SERIAL PRIMARY KEY,
+    user_id INT UNIQUE NOT NULL REFERENCES login_credentials(id) ON DELETE CASCADE,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    phone VARCHAR(20) UNIQUE,
+    date_of_birth DATE,
+    gender VARCHAR(10),
+    country VARCHAR(100),
+    postal_code VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
